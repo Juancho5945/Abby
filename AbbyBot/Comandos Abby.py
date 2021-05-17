463d0544993227b9e7e78bcebbcb12cadd1f618f
Rex Juancho <75599353+Juancho5945@users.noreply.github.com>
2021-05-17 15:04:49 -0500

    Update Comandos Abby.py

--- AbbyBot/Comandos Abby.py
+++ AbbyBot/Comandos Abby.py
@@ -1,17 +1,19 @@
-///Comandos de AbbyBot///
+/// AbbyBot Commands ///
 
-'Comandos------------------------Alias-------------------Funcion'
-'nb#play-------------------------nb#p--------------------Reproducir musica'
-'nb#queue------------------------nb#playlist-------------Mostrar la lista del servidor (Debe tener al menos 2)'
-'nb#volume-----------------------nb#v--------------------Adjustar el volumen si ponen -1 no funcionanra el bot'
-'nb#skip-------------------------NaN---------------------Skippea la musica actual y reproduce la siguente'
-'nb#stop-------------------------nb#dc-------------------Detiene la musica y el bot se marcha del canal de voz'
-'nb#loop-------------------------NaN---------------------Repite una musica'
-'nb#loopqueue--------------------NaN---------------------Repite varias musicas'
-'nb#pause------------------------NaN---------------------Pausa la musica'
-'nb#resume-----------------------NaN---------------------Resume la musica'
-'nb#botinfo----------------------NaN---------------------Muestra la info del bot'
-'nb#help-------------------------NaN---------------------Comando de Ayuda'
-'nb#guide------------------------NaN---------------------Guia del bot'
+'Commands ------------------------ Aliases ------------------- Function'
+'nb#play ------------------------- nb#p -------------------- Play music'
+'nb#queue ------------------------ nb#playlist ------------- Show server list ( Must have at least 2) '
+'nb#volume ----------------------- nb#v -------------------- Adjust the volume if they put -1 the bot will not work '
+'nb#skip ------------------------- NaN --------------------- Skip the current music and play the next one '
+'nb#stop ------------------------- nb#dc ------------------- Stop the music and the bot leaves the voice channel '
+'nb#loop ------------------------- NaN --------------------- Repeat a music '
+'nb#loopqueue -------------------- NaN --------------------- Repeat several songs'
+'nb#pause ------------------------ NaN --------------------- Pause the music
+'nb#resume ----------------------- NaN --------------------- Resume music'
+'nb#botinfo ---------------------- NaN --------------------- Show the bot info '
+'nb#help ------------------------- NaN --------------------- Help Command '
+'nb#guide ------------------------ NaN --------------------- Bot guide '
 
-///Cada comando se ha hecho con una opcion alguna///
+/// Each command has been done with an option ///
+
+'File created in Visual Studio Code (VSC)'
