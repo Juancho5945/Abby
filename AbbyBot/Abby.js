0f1911068a91896e5a499f9dbedffb8068faec65
Rex Juancho <75599353+Juancho5945@users.noreply.github.com>
2021-05-26 09:33:55 -0500

    Update Abby.js

--- AbbyBot/Abby.js
+++ AbbyBot/Abby.js
@@ -3,10 +3,10 @@
 ('Some of you may ask me who is AbbyBot and how do you think it exists, well here is my answer')
 
 (1)/'Abby is a music bot that works with Aoi.js and node.js with its ytdl-core package.'
-(2)/'At the moment AbbyBot has 11 commands, 9 music commands and 3 help commands.'
+(2)/'At the moment AbbyBot has 13 commands, 10 music commands and 3 help commands.'
 (3)/'I know some may ask why the name Her real name is Ethereal Abby, better known as Abby.'
-(4)/'Abby's mission is for you to play music on your server even if you are bored.'
-(5)/'I've taken the time to think if more than 1 Abby why not 5 but I'll keep thinking.'
+(4)/'Abbys mission is for you to play music on your server even if you are bored.'
+(5)/'I ve taken the time to think if more than 1 Abby why not 5 but I ll keep thinking.'
 
 'Abby has a small database so that when you play music it stays in the queue for you to replay it over and over again.'
 
