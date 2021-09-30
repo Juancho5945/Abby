6ef97b5c82c75b4bd66ba803abdfef001fd26ef4
Rex Juancho <75599353+Juancho5945@users.noreply.github.com>
2021-09-29 22:12:58 -0500

    Update Comandos Abby.py

--- AbbyBot/Comandos Abby.py
+++ AbbyBot/Comandos Abby.py
@@ -1,18 +1,44 @@
-/// AbbyBot Commands ///
+/// Queen Abby Commands ///
 
-'Commands ------------------------ Aliases ------------------- Function'
-'nb#play ------------------------- nb#p -------------------- Play music'
-'nb#queue ------------------------ nb#playlist ------------- Show server list ( Must have at least 2) '
-'nb#volume ----------------------- nb#v -------------------- Adjust the volume if they put -1 the bot will not work '
-'nb#skip ------------------------- NaN --------------------- Skip the current music and play the next one '
-'nb#stop ------------------------- nb#dc ------------------- Stop the music and the bot leaves the voice channel '
-'nb#loop ------------------------- NaN --------------------- Repeat a music '
-'nb#loopqueue -------------------- NaN --------------------- Repeat several songs'
-'nb#pause ------------------------ NaN --------------------- Pause the music'
-'nb#resume ----------------------- NaN --------------------- Resume music'
-'nb#botinfo ---------------------- NaN --------------------- Show the bot info '
-'nb#help ------------------------- NaN --------------------- Help Command '
-'nb#guide ------------------------ NaN --------------------- Bot guide '
+ <play - command where you look for the song and play it
+ <spplay - Play music from Spotify & Soundcloud (Experimental)
+ <np - shows the song that is playing
+ <queue - you can see the playlist
+ <skip - change the song
+ <stop- stop the song
+ <volume - raise or lower the volume of your song
+ <loop - Repeat the music currently
+ <loopqueue - Repeat the full queue
+ <guide - Shows the complete guide of the bot and the function of the commands
+ <pause - Pause the song and interrupt it
+ <resume - Resume the paused song and continue with the current song in the queue
+ <botinfo - Shows the complete status of the bot servers users all
+ <about - About my little abby
+ <updates - Show Abby's updates
+ <vote - Vote for abby will be happy to support her
+ <ping - Demonstrates Abby's latency
+ <uptime - Show how long Abby has been on
+ <abby - Mmm... A short text
+ <stats - The bot stats shows normal voice channels and emojis from all servers
+ <prune - It consists in that when the embed of nowplaying is activated when the music is finished it is deleted avoiding the flood
+ <join - The bot joins the voice channel where you are
+ <leave - The bot will go away if you use it
+  
+/// Aliases ///
+
+ <play [AKA: p]
+ <spplay [AKA: spsp]
+ <queue [AKA: playlist]
+ <volume [AKA: v]
+ <updates [AKA: up]
+ <ping [AKA: ing]
+ <uptime [AKA: ime]
+  
+/// Administrative and Owner Commands ///
+
+ <setprefix - Change the prefix to your liking [NEW] = [Permissions required: ADMINISTRATOR]
+ <eval - Command to evaluate an embed command [Permission Required: Sunky Only]
+ <blacklist - Owner forbids users to use his bot for breaking his rules [Permission required: Sunky only] [Blacklisted users cannot use the bot]
 
 /// Each command has been done with an option ///
 
